6be1a308b937649644a84543dcaa0bc08539dd32
nrd141913 <nrd141913@gmail.com>
2021-04-01 22:04:36 +0600

    server added

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 const uri = `mongodb+srv://durjoybd14:naim14@cluster0.ghclx.mongodb.net/burjAlArab?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-const admin = require('firebase-admin');
-const port = 4000;
+// const admin = require('firebase-admin');
+const port = 5000;
 
 
 const app = express();
@@ -15,12 +15,12 @@ app.use(express.json());
 //admin
 
 
-var serviceAccount = require("./configs/burj-al-arab-lite-1-firebase-adminsdk-w2s11-ff49bb7f45.json");
+// var serviceAccount = require("./configs/burj-al-arab-lite-1-firebase-adminsdk-w2s11-ff49bb7f45.json");
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    // databaseURL: process.env.FIRE_DB
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     // databaseURL: process.env.FIRE_DB
+// });
 
 
 
@@ -35,45 +35,62 @@ client.connect(err => {
             })
         console.log(newBookings)
     })
-    app.get('/bookings', (req, res) => {
-        const bearer = req.headers.authorization;
-        console.log(bearer);
-        if (bearer && bearer.startsWith('Bearer ')) {
-            const idToken = bearer.split(' ')[1];
-            console.log({ idToken });
-            // idToken comes from the client app
-            admin.auth().verifyIdToken(idToken)
-                .then((decodedToken) => {
-                    const tokenEmail = decodedToken.email;
-                    if (tokenEmail === req.query.email) {
-                        bookings.find({ email: req.query.email }) //objectname:query
-                            .toArray((err, documents) => {
-                                res.status(200).send(documents)
 
-                            })
-                    }
-                    else {
-                        res.status(401).send('unauthorized access')
+//not work
+//     app.get('/bookings', (req, res) => {
+//         const bearer = req.headers.authorization;
+//         console.log(bearer);
+//         if (bearer && bearer.startsWith('Bearer ')) {
+//             const idToken = bearer.split(' ')[1];
+//             console.log({ idToken });
+//             // idToken comes from the client app
+//             admin.auth().verifyIdToken(idToken)
+//                 .then((decodedToken) => {
+//                     const tokenEmail = decodedToken.email;
+//                     if (tokenEmail === req.query.email) {
+//                         bookings.find({ email: req.query.email }) //objectname:query
+//                             .toArray((err, documents) => {
+//                                 res.status(200).send(documents)
 
-                    }
-                    // ...
-                })
-                .catch((error) => {
-                    // Handle error
-                    res.status(401).send('unauthorized access')
+//                             })
+//                     }
+//                     else {
+//                         res.status(401).send('unauthorized access')
 
-                });
+//                     }
+//                     // ...
+//                 })
+//                 .catch((error) => {
+//                     // Handle error
+//                     res.status(401).send('unauthorized access')
 
-        }
-        else {
-            res.status(401).send('unauthorized access')
-        }
+//                 });
+
+//         }
+//         else {
+//             res.status(401).send('unauthorized access')
+//         }
 
 
+//     }) ///not working
+
+// get
+
+app.get('/bookings', (req, res)=>{
+    bookings.find({email: req.query.email})
+    .toArray((err , documents)=>{
+        res.send(documents)
     })
+})
+
+
 });
 
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+  })
 
 
 app.listen(port, () => {
